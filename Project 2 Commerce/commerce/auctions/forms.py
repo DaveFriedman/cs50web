11886b67006814802865f4cbae5a536994684fa0
Dave Friedman <davemfriedman@gmail.com>
2022-01-22 19:12:44 -0500

    created models, forms

---
+++ Project 2 Commerce/commerce/auctions/forms.py
@@ -0,0 +1,62 @@
+from django.forms import ModelForm
+from django.utils.translation import gettext_lazy as _
+
+from .models import Listing, Bid, Comment
+
+
+class ListingForm(ModelForm):
+    class Meta:
+        mode = Listing
+        # Are fields lists[] or tuples()? confusing:
+        # https://docs.djangoproject.com/en/4.0/topics/forms/modelforms/
+        
+        fields = ["name", "quantity", "list_price", "ship_price", "buynow_price"
+                  ,"description","listing_timeout", "is_returnable"]
+        
+        labels = {
+            "name": _("Item name"),
+            "quantity": _("Item quantity"),
+            "list_price": _("Minimum bid"),
+            "ship_price": _("Shipping cost"),
+            "buynow_price": _("Buy now bid"),
+            "description": _("Item description"),
+            "listing_timeout": _("Auction length")
+            }
+
+        help_texts = {
+            "is_returnable": _("Check if item is returnable"),
+            "listing_timeout": _("(in days)")
+        }
+
+        error_messages = {
+            "quantity": _("Quantity must be greater than zero"),
+            "list_price": _("Price must be greater than zero"),
+            "buynow_price": _("Price must be greater than zero"),
+            "listing_timeout": _("Auction must be at least 1 day")
+        }
+
+
+class BidForm(ModelForm):
+    class Meta:
+        model = Bid
+        fields = ["bid_price"]
+
+
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ["comment"]
+
+
+
+# class entry_form(forms.Form):
+
+#     title = forms.CharField(
+#         initial="title",
+#         label="Title",
+#         widget=forms.TextInput()
+#         )
+#     body = forms.CharField(
+#         label="Body", 
+#         widget=forms.Textarea()
+#         )
--- Project 2 Commerce/commerce/auctions/models.py
+++ Project 2 Commerce/commerce/auctions/models.py
@@ -4,3 +4,68 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Listing(models.Model):
+
+    name = models.CharField(max_length=64)
+    quantity = models.IntegerField(default=1)
+    is_returnable = models.BooleanField()
+    description = models.TextField(max_length=480)
+
+    list_price = models.DecimalField(decimal_places=2)
+    buynow_price = models.DecimalField(decimal_places=2)    
+    ship_price = models.DecimalField(decimal_places=2)
+
+    listing_start = models.DateTimeField(auto_now_add=True)
+    listing_end = models.DateTimeField()
+    listing_timeout = models.DateTimeField()
+
+    lister = models.ForeignKey('User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Listing {self.id}: {self.lister}'s {self.name}"
+
+    def verbose(self):
+        return f"Listing {self.id}: {self.lister}'s ({self.quantity}) \
+                {self.name}, returnable={self.is_returnable} {chr(10)} \
+                listed on {self.listing_start} for ${self.list_price} + \
+                ${self.ship_price}, {chr(10)} \
+                times out {self.listing_timeout}, buynow ${self.buynow_price} \
+                hit at {self.listing_end} {chr(10)} \
+                description: {self.description}"
+
+
+class Bid():
+
+    bid_price = models.DecimalField(decimal_places=2)
+    bid_time = models.DateTimeField(auto_now_add=True)
+    bidder = models.ForeignKey('User', on_delete=models.CASCADE)
+    listing = models.ForeignKey('Listing', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Bid {self.id}: {self.bid_price} on {self.listing}"
+        
+    def verbose(self):
+        return f"Bid {self.id}: {self.bidder} bid {self.bid_price} on  \
+                {self.listing} at {self.bid_time}"
+
+
+class Comment():
+
+    comment = models.TextField(max_length=280)
+    is_hidden = models.BooleanField()
+
+    commented = models.DateTimeField(auto_now_add=True)
+    commenter = models.ForeignKey('User')
+
+    listing = models.ForeignKey('Listing', on_delete=models.CASCADE)
+    parent = models.ForeignKey('self')
+
+    def __str__(self):
+        return f"Comment {self.id}: {self.commenter} posted on {self.listing}"
+
+    def verbose(self):
+        return f"Comment {self.id}: At {self.commented}, on {self.listing}, \
+                {self.commenter} replied to {self.parent}, saying: {chr(10)} \
+                {self.comment} {chr(10)} (hidden={self.is_hidden})"

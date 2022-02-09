37df677605404aba11fb6e3cb37f046efd66e2c6
Dave Friedman <davemfriedman@gmail.com>
2022-02-08 20:37:20 -0500

    Black formatted context_processors.py

--- Project 2 Commerce/commerce/auctions/context_processors.py
+++ Project 2 Commerce/commerce/auctions/context_processors.py
@@ -4,7 +4,4 @@ CATEGORIES = Listing._meta.get_field('category').choices
 
 # https://stackoverflow.com/questions/34902707/how-can-i-pass-data-to-django-layouts-like-base-html-without-having-to-provi
 def add_categories_to_context(request):
-    return {
-        'categories': CATEGORIES
-    }
-
+    return {"categories": CATEGORIES}

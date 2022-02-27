57819a7240f76b8ba12fbb88b31c753171501b21
Dave Friedman <davemfriedman@gmail.com>
2022-02-27 17:58:27 -0500

    send emails via api, show emails via api, favicon

--- Project 3 Mail/mail/mail/static/mail/inbox.js
+++ Project 3 Mail/mail/mail/static/mail/inbox.js
@@ -6,12 +6,13 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
+  document.querySelector('#compose-form').onsubmit = send_email;
+  
   // By default, load the inbox
   load_mailbox('inbox');
 });
-
+  
 function compose_email() {
-
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
@@ -22,12 +23,97 @@ function compose_email() {
   document.querySelector('#compose-body').value = '';
 }
 
+function send_email() {
+  const formElement = document.forms['compose-form'];
+  const formData = new FormData(formElement);
+
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: formData.get('compose-recipients'),
+        subject: formData.get('compose-subject'),
+        body: formData.get('compose-body')
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+      console.log(result);
+  })
+  .catch((error) => console.log(error));
+
+  return false;
+}
+
 function load_mailbox(mailbox) {
-  
+
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // Fetch emails
+  fetch('/emails/inbox')
+  .then(response => response.json())
+  .then(json => {
+
+    // Print emails    
+    const mailbox_email_div = document.querySelector('#emails-view');
+    json.forEach(email => {
+
+      if (email.archived === false) {
+        const mailbox_email = show_mailbox_email(email);
+        if (email.unread === true) {
+          // TODO
+        }
+        mailbox_email_div.append(mailbox_email);
+      }
+    });
+  });
 }
+
+function show_mailbox_email(email) {
+  console.log(email);
+
+  let mailbox_email = document.createElement('div');
+  mailbox_email.className = "row";
+
+  let address = document.createElement('div');
+  address.className = "address col-3";
+  address.textContent = `${email.sender}`;
+  mailbox_email.append(address);
+
+  let subject = document.createElement('div');
+  subject.className = "subject col-6";
+  subject.textContent = email.subject.concat("  ");
+  mailbox_email.append(subject);
+
+  let body = document.createElement('span');
+  body.className = "body font-weight-light";
+  body.textContent = (email.body.length <= 10) ? email.body : email.body.slice(0, 10).concat("...");
+  subject.append(body);
+
+  let timestamp = document.createElement('div');
+  timestamp.className = "timestamp col-3";
+  timestamp = email.timestamp;
+  mailbox_email.append(timestamp);
+
+  return mailbox_email;
+}
+
+
+
+
+
+
+
+//         let emailDiv = document.createElement('li');
+// archived
+// body
+// id
+// read
+// recipients
+// sender
+// subject
+// timestamp

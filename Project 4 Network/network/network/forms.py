69d446fed7f2ae8486e3fbb6d7317b083be007cb
Dave Friedman <davemfriedman@gmail.com>
2022-03-10 20:10:29 -0500

    first pass on models, migrations, forms

---
+++ Project 4 Network/network/network/forms.py
@@ -0,0 +1,10 @@
+from django.forms import ModelForm, Textarea
+
+from .models import Post
+
+class PostForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = ["body"]
+        labels = {"body" : "What's on your mind?"}
+        widgets = {"body" : Textarea(attrs={"rows": 6})}
--- Project 4 Network/network/network/models.py
+++ Project 4 Network/network/network/models.py
@@ -1,6 +1,33 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
+from django.db.models import *
 
 
 class User(AbstractUser):
     pass
+
+
+class Post(Model):
+    author = ForeignKey(User, on_delete=CASCADE)
+    body = TextField(max_length=280)
+    timestamp = DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Post {self.id}: by {self.author} at {self.timestamp}"
+
+
+class Like(Model):
+    post = ForeignKey(Post, on_delete=CASCADE)
+    liker = ForeignKey(User, on_delete=CASCADE)
+    timestamp = DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.id}: at {self.timestamp}, {self.liker} liked {self.post}"
+
+
+class Follow(Model):
+    creator = ForeignKey(User, on_delete=CASCADE, related_name="creator")
+    follower = ForeignKey(User, on_delete=CASCADE, related_name="follower")
+    timestamp = DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.id}: {self.follower} follows {self.creator}"

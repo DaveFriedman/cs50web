abb5c9937a040614bf9ca40c28c9e8da17830a61
Dave Friedman <davemfriedman@gmail.com>
2022-03-14 16:25:06 -0400

    settings, profile, feed skeletons

--- Project 4 Network/network/network/urls.py
+++ Project 4 Network/network/network/urls.py
@@ -5,7 +5,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("feed", views.feed, name="feed"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+    path("settings", views.account_settings, name="settings"),
+    path("u/<int:userid>/<str:username>", views.account_profile, name="profile"),
 ]
--- Project 4 Network/network/network/views.py
+++ Project 4 Network/network/network/views.py
@@ -1,14 +1,80 @@
+from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.utils import timezone
 
-from .models import User
+from .models import User, Post, Like, Follow
+from .forms import PostForm
+
+# TODO
+# Create create_post.html
+# Create some dummy data
+# create post editor
+# Fill out User attributes? (profile pic, email uniqueness, about me)
+# Add last-edit timestamp to Post?
+# pagination
 
 
 def index(request):
     return render(request, "network/index.html")
+
+
+def feed(request):
+    return render(request, "network/feed.html", {
+        "posts" : Post.objects.all().order_by("-id")
+    })
+
+
+@login_required
+def create_post(request):
+    if request.method == "POST":
+        form= PostForm(request.POST)
+
+        if form.is_valid():
+            try:
+                new_post = Post.objects.create(
+                    body = form.cleaned_data["body"],
+                    author = request.user,
+                    timestamp = timezone.now()
+                )
+                new_post.save()
+            except IntegrityError as e:
+                messages.error(request, f"{e.__cause__}")
+                return HttpResponseRedirect(reverse("index"))
+    else:
+        return HttpResponseRedirect(reverse("index"))
+
+
+@login_required
+def account_settings(request):
+    userinfo = {
+        "username": request.user.username,
+        "first" : request.user.first_name,
+        "last" : request.user.last_name,
+        "email" : request.user.email,
+        "last_login" : request.user.last_login,
+        "date_joined" : request.user.date_joined
+    }
+    # userinfo = request.user
+    return render(request, "network/account_settings.html", {
+        "userinfo": userinfo
+        })
+
+
+@login_required
+def account_profile(request, userid, username):
+    user = User.objects.get(id = userid)
+    return render(request, "network/account_profile.html", {
+        "user" : user,
+        "follower_count" : Follow.objects.filter(creator=user).count(),
+        "following_count" : Follow.objects.filter(follower=user).count(),
+        "posts" : Post.objects.filter(author=user).order_by("-id")
+    })
+
 
 
 def login_view(request):
